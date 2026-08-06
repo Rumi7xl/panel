981e2684c79b64836249e0fcae7f1a501883955d
Rumi7xl <rumi7xl@gmail.com>
2026-08-06 04:58:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,78 +7,53 @@ app.use(cors());
 app.use(express.json());
 
 
-// TEST
-app.get("/", (req, res) => {
+app.get("/", (req,res)=>{
     res.send("RUMİ7XL PANEL API Aktif 🚀");
 });
 
 
-
-// PANEL VERİLERİ
-app.get("/api/stats", (req, res) => {
+app.get("/api/stats",(req,res)=>{
 
     res.json({
 
-        profile: {
-            name: "RUMİ7XL",
-            owner: "Bercan Yakar"
+        profile:{
+            name:"RUMİ7XL",
+            owner:"Bercan Yakar"
         },
 
-
-        kick: {
-
-            followers: 12458,
-
-            subscribers: 325,
-
-            latestFollowers: [
-
+        kick:{
+            followers:12458,
+            subscribers:325,
+            latestFollowers:[
                 "Yeni Takipçi 1",
                 "Yeni Takipçi 2",
                 "Yeni Takipçi 3",
                 "Yeni Takipçi 4",
                 "Yeni Takipçi 5"
-
             ]
-
         },
 
-
-        youtube: {
-
-            subscribers: 5420
-
+        youtube:{
+            subscribers:5420
         },
 
-
-        tiktok: {
-
-            followers: 25800
-
+        tiktok:{
+            followers:25800
         },
 
-
-        instagram: {
-
-            followers: 0
-
+        instagram:{
+            followers:0
         },
 
-
-        lastUpdate: new Date()
+        lastUpdate:new Date()
 
     });
 
 });
 
 
+const PORT = process.env.PORT || 10000;
 
-// Render PORT
-const PORT = process.env.PORT || 3000;
-
-
-app.listen(PORT, () => {
-
+app.listen(PORT,()=>{
     console.log("RUMİ7XL PANEL API Çalışıyor 🚀");
-
 });
