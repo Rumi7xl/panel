22341448ce75ab1fd9f7ebb0ed5c5890f1daf022
Rumi7xl <rumi7xl@gmail.com>
2026-08-06 05:28:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ console.log("SERVER DOSYASI OKUNDU");
 
 const express = require("express");
 const cors = require("cors");
-
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-// HTML PANELİ GÖSTER
+// HTML PANEL
 app.use(express.static(__dirname));
 
 
@@ -21,45 +21,36 @@ app.get("/", (req,res)=>{
 });
 
 
-// API TEST
+// TEST
 app.get("/test",(req,res)=>{
     res.send("TEST ÇALIŞIYOR 🚀");
 });
 
 
-// PANEL VERİLERİ
+
+// DATABASE OKUMA
+function getDatabase(){
+
+    const data = fs.readFileSync(
+        "./database.json",
+        "utf8"
+    );
+
+    return JSON.parse(data);
+
+}
+
+
+
+// PANEL API
 app.get("/api/stats",(req,res)=>{
+
+    const data = getDatabase();
+
 
     res.json({
 
-        profile:{
-            name:"RUMİ7XL",
-            owner:"Bercan Yakar"
-        },
-
-        kick:{
-            followers:12458,
-            subscribers:325,
-            latestFollowers:[
-                "Yeni Takipçi 1",
-                "Yeni Takipçi 2",
-                "Yeni Takipçi 3",
-                "Yeni Takipçi 4",
-                "Yeni Takipçi 5"
-            ]
-        },
-
-        youtube:{
-            subscribers:5420
-        },
-
-        tiktok:{
-            followers:25800
-        },
-
-        instagram:{
-            followers:0
-        },
+        ...data,
 
         status:"online",
 
@@ -67,15 +58,20 @@ app.get("/api/stats",(req,res)=>{
 
     });
 
+
 });
 
 
-// RENDER PORT
+
+
+// PORT
 const PORT = process.env.PORT || 10000;
 
 
 app.listen(PORT,()=>{
 
-console.log(`RUMİ7XL PANEL API Çalışıyor 🚀 Port: ${PORT}`);
+console.log(
+`RUMİ7XL PANEL API Çalışıyor 🚀 Port: ${PORT}`
+);
 
 });
